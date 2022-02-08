487569028e95782cd8a1f71ca5a3a6b88febfdfa
nosike22 <frank.nosike@gmail.com>
2022-02-08 16:29:16 +0100

    Checking number code

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Checking_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+            Console.WriteLine("Enter Number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+             result = num1 + num2;
+              Console.ReadKey();ghh
+
+            if(result < 100)
+            {
+                Console.WriteLine("True");
+
+            }
+               else if( result > 100)
+            {
+                Console.WriteLine("False");
+                Console.WriteLine("Press Enter to exit");
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
